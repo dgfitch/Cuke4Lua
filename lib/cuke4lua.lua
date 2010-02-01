c61e6b64820619847bf7e93ef58a3da38313b604
Dan Fitch <dgfitch@gmail.com>
2010-01-31 20:26:32 -0600

    Adding json lib and parsing

--- lib/cuke4lua.lua
+++ lib/cuke4lua.lua
@@ -1,4 +1,5 @@
 local socket = require "socket"
+require "json"
 
 host = "127.0.0.1"
 port = arg[2] or 3901
@@ -13,15 +14,32 @@ local log = logging.console("%date %level %message\n")
 --require "logging.file"
 --local log = logging.file("test%s.log", "%Y-%m-%d", "%date %level %message\n")
 
-log:info("Starting cuke4lua server on host %s port %s", host, port)
+log:info(string.format("Starting cuke4lua server on host %s port %s", host, port))
 
 server = assert(socket.bind(host, port))
 server:settimeout(60)
 conn = server:accept()
-repeat
-  data, e = conn:receive()
-  log:info(data)
-until not e
-log:warn(e)
 
+local skip = conn:receive()
+
+input = function(data)
+  local o = json.decode(data)
+end
+
+local e
+repeat
+  local data,e = conn:receive()
+
+  if data then
+    if pcall(function () input(data) end) then
+      log:info("JSON message received: " .. data)
+    else
+      log:warn("JSON parse error for: " .. data)
+    end
+  end
+until data == nil
+
+if e then
+  log:warn("Connection error: " .. e)
+end
 log:info("Shutting down server")
